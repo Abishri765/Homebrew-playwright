63e86e7a262ad42bd8b0a5554e9b06a06c1d0ff9
Abishri765 <beingshriabi@gmail.com>
2023-11-20 18:16:26 +0530

    first commit

--- playwright.rb
+++ playwright.rb
@@ -1,10 +1,10 @@
+
 class Playwright < Formula
   desc ""
   homepage ""
   url "https://github.com/Abishri765/playwright/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "482ae924a570034e3d16835c0ba2ea81643ea85d13b6f290426644d3c1c0d401"
   license ""
-
   depends_on "node@18" # Use Node.js 18.17.1
 
   def install
